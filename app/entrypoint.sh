7f695ab3cd938d6f9210e3ae62fa09adeefc6b7f
artpods56 <artpods56@gmail.com>
2024-06-14 15:39:51 +0200

    chore: converted entrypoint.sh to UNIX/OSX format to deal with '\r' character errors during script execution

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,23 +2,12 @@
 
 set -e
 
-if [ "$DATABASE" = "postgres" ]; then
-    echo "Waiting for postgres..."
-
-    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
-      sleep 1
-    done
-
-    echo "PostgreSQL started"
-    
-fi
-
 echo "Making migrations..."
-python manage.py makemigrations --noinput    
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-    
+
 echo "Collecting static files..."
-python manage.py collectstatic --no-input --clear    
+python manage.py collectstatic --no-input --clear
 
 echo "Creating superuser..."
 python create_superuser.py
